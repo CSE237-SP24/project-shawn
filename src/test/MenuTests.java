c73866346d6a930f1d29470fda9401d92ac52488
jiabeihan <h.jiabei@wustl.edu>
2024-03-18 12:52:05 -0500

    Finished implementation (need to work on tests)

--- src/test/MenuTests.java
+++ src/test/MenuTests.java
@@ -1,6 +1,8 @@
 package test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,15 +10,6 @@ import bankapp.BankAccount;
 import bankapp.Menu;
 
 class MenuTests {
-
-	@Test
-	void testUserDeposit() {
-		Menu m = new Menu();
-		//user has provided value input of 50
-		m.processingUserSelection(50);
-		
-		BankAccount account = m.getAccount();
-		assertEquals(50, account.getBalance(), 0.01);
-	}
-
+	
+	
 }
